f9b8526255c2f61e09f35df2eec7d84ea2af5126
Abdullahi <abdullahiabdulwasiu1@gmail.com>
2024-03-25 11:15:43 +0000

    Complete Basic Chat

--- actions/actions.py
+++ actions/actions.py
@@ -4,96 +4,116 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
 
-AVAILABLE_LAPTOP_BRANDS = ["hp", "dell", "lenovo", "apple", "alienware", "asus"]
-AVAILABLE_LAPTOP_PROCESSORS = ["intel celeron", "intel core i3", "intel core i5", "intel core i7", "intel core i9"]
-AVAILABLE_OS_TYPES = ["windows", "macbook", "mac", "linux"]
+import random
 
+def shuffle_response_template(templates):
+    return random.choice(templates)
+    
 
-class AskForLaptopToachScreen(Action):
-    """Ask weather laptop be touch screen. In Second laptop form"""
-    """Programmatically generate a button that a user can press"""
-    """Button in rasa interactive """
+class  ActionValidateSlot(Action): 
 
     def name(self) -> Text:
-        return "action_ask_laptop_touch_screen"
+        return "action_laptop_uses"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        specify_slots = tracker.get_slot("laptop_use")
+
+        if specify_slots != None:
+            specify_slot = " ".join(specify_slots)
+
+            response = domain["responses"]["utter_laptop_use"][0]
+            response_text = response["text"].format(laptop_use=specify_slot)
+            dispatcher.utter_message(text=response_text)
+        else:
+            dispatcher.utter_message(text=f"We have some good products checkout: <DB data latest product>")
+        return []
+
+
+class  ActionValidateLaptopUseAndBudget(Action): 
+
+    def name(self) -> Text:
+        return "action_laptop_recommend"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(
-            text="Would you like to order a touch screen laptop?",
-            buttons=[
-                {"title": "yes", "payload": "/affirm"},
-                {"title": "no", "payload": "/deny"},
-            ],
-            
-            )
+        specify_slots = tracker.get_slot("laptop_use")
+        specify_budget = tracker.get_slot("laptop_budget")
+        # specify_slot = " ".join(specify_slots)
+       
+            # samsung_entity = next(tracker.get_latest_entity_values("phone"), None)
+        
+        
+        if specify_slots != None:
+            response = domain["responses"]["utter_recommend_laptop_if_user_give_use"][0]
+            response_text = response["text"].format(laptop_use=type(specify_slots))
+        if specify_budget != None:
+            response = domain["responses"]["utter_recommend_laptop_if_user_give_budget"][0]
+            if int(specify_budget) < 200:
+                specify_budget = "small budget < $400"
+            response_text = response["text"].format(budget=specify_budget)
+        if specify_budget and specify_slots != None:
+            for slot in specify_slots:
+                specify_slot = slot + " "
+            response = domain["responses"]["utter_recommend_laptop_if_user_give_use_and_budget"][0]
+            response_text = response["text"].format(laptop_use=specify_slot, budget=specify_budget)
+       
+        
+        dispatcher.utter_message(text=response_text)
         return []
 
 
-class ValidateSecondLaptopForm(FormValidationAction):
+class  ActionGetQueryBrand(Action): 
+    """Get user query for a particullar brand"""
     def name(self) -> Text:
-        return "validate_second_laptop_form"
+        return "action_get_available_brand"
 
-    def validate_laptop_os(
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        response = [
+            domain["responses"]["utter_brand_availability_and_stock"][0],
+            domain["responses"]["utter_brand_availability_and_stock"][1]
+        ]
+        response = shuffle_response_template(response)
+        specify_brand = tracker.get_slot("query_brand")
+        response_text = response["text"].format(query_brand="")
+        if specify_brand != None:
+            response_text = response["text"].format(query_brand=specify_brand)
+        dispatcher.utter_message(text=response_text)
+        return []
+
+
+
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    async def run(
         self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher, 
+        dispatcher: CollectingDispatcher,
         tracker: Tracker,
-        domain: DomainDict
-        ) -> Dict[Text, Any]:
-        """Validate `laptop os` value."""
-        if slot_value.lower() not in AVAILABLE_OS_TYPES:
-            dispatcher.utter_message(text="We dont have your specify OS")
-            return {"laptop_os": None}
-        dispatcher.utter_message(text=f"Ok! You choose a laptop with {slot_value} OS")
-        return {"laptop_os": slot_value}
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
-    def validate_laptop_touch_screen(
-        self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker,
-        domain: DomainDict
-        ) -> Dict[Text, Any]:
-        """Validate `laptop screen` value."""
-
-        if tracker.get_intent_of_latest_message() == "affirm":
-            dispatcher.utter_message(text="I will remember you prefer a touch screen laptop")
-            return {"laptop_touch_screen": True}
-
-        if tracker.get_intent_of_latest_message() == "deny":
-            dispatcher.utter_message(text="I will remember that you dont want a touch screen laptop")
-            return {"laptop_touch_screen": False}
-
-        dispatcher.utter_message(text="I did not get that")
-        return {"laptop_touch_screen": None}
-
-    def validate_laptop_brand(
-        self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker,
-        domain: DomainDict
-        ) -> Dict[Text, Any]:
-        """Validate `laptop brand` value."""
-        if slot_value.lower() not in AVAILABLE_LAPTOP_BRANDS:
-            dispatcher.utter_message(text=f"We dont have your specify Brand right now. Choose {'/'.join(AVAILABLE_LAPTOP_BRANDS)}")
-            return {"laptop_brand": None}
-        dispatcher.utter_message(text=f"Ok! You choose a {slot_value} laptop")
-        return {"laptop_brand": slot_value}
-
-    def validate_laptop_processor(
-        self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker,
-        domain: DomainDict
-        ) -> Dict[Text, Any]:
-        """Validate `laptop processor` value."""
-        if slot_value.lower() not in AVAILABLE_LAPTOP_PROCESSORS:
-            dispatcher.utter_message(text="We dont have your specify processor")
-            return {"laptop_processor": None}
-        dispatcher.utter_message(text=f"Good! You choose a laptop with {slot_value} processor")
-        return {"laptop_processor": slot_value}
+        response = [ 
+            domain["responses"]["utter_default_one"][0],
+            domain["responses"]["utter_default_one"][1]
+        ]
+        response = shuffle_response_template(response)
+        dispatcher.utter_message(text=response)
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
